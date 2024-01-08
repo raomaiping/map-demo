dde882e4eecfa59790c5295451c4ec8a634656fc
raomaiping <2582395486@qq.com>
2024-01-09 04:15:19 +0800

    包 问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,4 @@
 import { LeafLet, OpenLayers } from './getSidebar'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
 export default {
   outDir: '../dist',
   title: 'Map Demo',
